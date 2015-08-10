6f3f8861ec964e646f0eec63f62d1e29b362c71a
Kevin Lanzon <kevinlanzon@gmail.com>
2015-08-10 19:54:20 +0100

    adds https to jquery cdn

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
